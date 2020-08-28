cf45e07c6c13bad816f4887dee35e2bbdcd7b0bb
Feng Lv <lvfeng@FengdeMacBook-Pro.local>
2020-08-28 18:01:00 +0800

    update, not finished

---
+++ test/mem_test.go
@@ -0,0 +1,15 @@
+package test
+
+// version 1
+
+// bash-5.0# free
+//               total        used        free      shared  buff/cache   available
+// Mem:      197980164    60246396     2767444     4340988   134966324   182142124
+// Swap:      16777212           0    16777212
+
+// version 2
+
+// 			total       used       free     shared    buffers     cached
+// Mem:     197980164  195483432    2496732    4340996       3200  134976012
+// -/+ buffers/cache:   60504220  137475944
+// Swap:     16777212          0   16777212
